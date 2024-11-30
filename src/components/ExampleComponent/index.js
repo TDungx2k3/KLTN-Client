1b9b4e21df45134b9d632acc3223945cb6bbe82f
TDungx2k3 <dungpgpma2k3@gmail.com>
2024-11-30 16:11:47 +0700

    base fe

---
+++ src/components/ExampleComponent/index.js
@@ -0,0 +1,26 @@
+
+import React, { Fragment } from 'react';
+
+import style from "./ExampleComponent.module.scss"
+import clsx from 'clsx';
+
+
+export const ExampleComponent = () => {
+    // code js
+
+    return(
+        // code html
+        <Fragment>
+            <div className={clsx(style.container)}>
+
+            </div>
+            <div>
+
+            </div>
+        </Fragment>
+        
+    );
+
+}
+
+export default ExampleComponent
---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+// all users
+const publicRoutes = [
+
+];
+
+// users after log in
+const customerRoutes = [
+
+];
+
+// only for admin role
+const adminRoutes = [
+
+];
+
+export { publicRoutes, adminRoutes, customerRoutes }
